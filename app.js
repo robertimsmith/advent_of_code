ee0d65340a89bde2c008e3e97e9713a0b3e305ee
Robert Smith <robert.smith@skyscanner.net>
2020-12-03 19:03:17 +0000

    question 3, parts 1 and 2

--- app.js
+++ app.js
@@ -1,42 +1,50 @@
-const { count } = require("console");
 var fs = require("fs");
-const { parserConfiguration } = require("yargs");
 var text = fs.readFileSync("./input_2020_Q3.txt", "utf8");
 let output = text.split("\n");
 
-console.log(output);
+slopes = [1,1,3,1,5,1,7,1,1,2];//2, 7, 3, 4, and 2
+answer = [0,0,0,0,0];
 
-let size = 11;
-
-let right = 3;
-let down = 1;
-let x = 0;
-let y = 0;
-
-let counter = 0;
-let rightBoundary = output[0].length; 
-console.log(rightBoundary);
-let bottomRow = output.length;
-console.log(bottomRow);
-
-while (y < bottomRow){
-    let hit = output[y].charAt(x);
-    if (hit === '#'){
-        counter++;
-    }
-
-    console.log(output[y], x, y, hit, counter);
-    
-    x += right;
-    y += down;
-    if (x>rightBoundary){
-        x -= rightBoundary;
-    }
-
+for (i = 0; i <= 4; i++){
+    let a = i * 2;
+    let b = i * 2 + 1;
+    answer[i] = calc(slopes[a], slopes[b]);   
 }
-for(y = 0; output.length; y++){
-    console.log(output[y] + " " + y);
-}
-console.log(counter);
+let final = 1; 
+for (i = 0; i <= 4; i++){
+    final = final * answer[i];
+} 
+console.log(final);
 
-//not 145 too low
+function calc(right, down){
+    let x = 0;
+    let y = 0;
+
+    let counter = 0;
+    let rightBoundary = output[0].length; 
+    //console.log(rightBoundary);
+    let bottomRow = output.length;
+    //console.log(bottomRow);
+
+    while (y < bottomRow){
+        let hit = output[y].charAt(x);
+        if (hit === '#'){
+            counter++;
+        }
+
+        //console.log(output[y] + " " + x  + " " +  y  + " " +  hit  + " " +  counter);
+        
+        x += right;
+        y += down;
+        if (x>=rightBoundary){
+            x -= rightBoundary;
+        }
+
+    }
+//    for(y = 0; y < output.length; y++){
+//        console.log(output[y] + " " + y);
+//    }
+    return (counter);
+}
+// part 1 151
+// part 2 7540141059
